a209f01f9fe83beb810732a5d49350c90c5bb5f3
Archit <archit.agarwal@swiggy.in>
2022-02-13 01:46:47 +0530

    hello world init

---
+++ src/com/hashcode/attempt/Application.java
@@ -0,0 +1,13 @@
+package com.hashcode.attempt;
+
+/**
+ * @author Archit Agarwal on 13/02/22
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to hashcode - One Pizza!!");
+    }
+
+}
