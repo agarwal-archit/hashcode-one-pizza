42029e443ce66bbcf31eafe4b8ffa36c481c2687
Utkarsh <utkarshagarwal3112@gmail.com>
2022-02-13 02:04:22 +0530

    temp

--- src/com/hashcode/attempt/Application.java
+++ src/com/hashcode/attempt/Application.java
@@ -8,6 +8,7 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("Welcome to hashcode - One Pizza!!");
+        System.out.println("welcome to google hashcode");
     }
 
 }
